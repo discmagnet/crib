852d62d5afd9200f8ebe5272a234d63a806296d3
discmagnet <kkumbier13@gmail.com>
2018-06-08 00:59:03 -0400

    function now determines rank, value, and suit

--- scored.R
+++ scored.R
@@ -1,4 +1,4 @@
-scored <- function(card1,card2,card3,card4,flipped){
+scored <- function(c1,c2,c3,c4,c5){
   # ==================================================================================
   # The "scored" function calculates the total points in a standard cribbage hand
   # after the player discards. Inputs are the 4 remaining cards in the player's hand
@@ -10,33 +10,78 @@ scored <- function(card1,card2,card3,card4,flipped){
   #   - example = Eight of hearts ~ 8h
   
   # ==================================================================================
+  # Determine Ranks
+  r1 <- substr(c1,1,1)
+  r2 <- substr(c2,1,1)
+  r3 <- substr(c3,1,1)
+  r4 <- substr(c4,1,1)
+  r5 <- substr(c5,1,1)
+  
+  # ==================================================================================
+  # Determine Values
+  value <- function(rank){
+    if(rank == "a") value <- 1
+    else if (rank == "2") value <- 2
+    else if (rank == "3") value <- 3
+    else if (rank == "4") value <- 4
+    else if (rank == "5") value <- 5
+    else if (rank == "6") value <- 6
+    else if (rank == "7") value <- 7
+    else if (rank == "8") value <- 8
+    else if (rank == "9") value <- 9
+    else if (rank == "t") value <- 10
+    else if (rank == "j") value <- 10
+    else if (rank == "q") value <- 10
+    else if (rank == "k") value <- 10
+  }
+  v1 <- value(r1)
+  v2 <- value(r2)
+  v3 <- value(r3)
+  v4 <- value(r4)
+  v5 <- value(r5)
+  
+  # ==================================================================================
   # Initialize Points to Zero
   points <- 0
   
   # ==================================================================================
-  # STEP 1: Determine Points from 15's
+  # Determine Points from 15's
   fifteens <- 0
   
   # ==================================================================================
-  # STEP 2: Determine Points from Pairs
+  # Determine Points from Pairs
   pair <- 0
   
   # ==================================================================================
-  # STEP 3: Determine Points from Runs (Lengths of 3, 4, and 5)
+  # Determine Points from Runs (Lengths of 3, 4, and 5)
   runs <- 0
   
   # ==================================================================================
-  # STEP 4:
-  suit1 <- substr(as.character(card1),2,2)
+  # Determine Suits
+  s1 <- substr(c1,2,2)
+  s2 <- substr(c2,2,2)
+  s3 <- substr(c3,2,2)
+  s4 <- substr(c4,2,2)
+  s5 <- substr(c5,2,2)
   
   # ==================================================================================
-  # STEP 5: Determine Presence of Flush
+  # Determine Presence of Flush
   flush <- 0
-  
+  if(s1 == s2 & s1 == s3 & s1 == s4){
+    if(s1 == s5){
+      flush <- 5
+    } else{
+      flush <- 4
+    }
+  } else{
+    flush <- 0
+  }
   # ==================================================================================
-  # STEP 6: Determine Presence of Knobs
+  # Determine Presence of Knobs
   knobs <- 0
   
+  # ==================================================================================
+  # Sum Total Points
   points <- points + fifteens + pair + runs + flush + knobs
-  return(suit1)
+  return(points)
 }
