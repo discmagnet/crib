ed5da2a1079479197e8e1b3ef8dcbfb36fa42a3c
discmagnet <kkumbier13@gmail.com>
2018-08-18 11:56:07 -0400

    working RShiny app

--- algorithm.R
+++ algorithm.R
@@ -1,78 +1,80 @@
-library(tidyr)
-library(utils)
-
-# Creating the 52-Card Deck
-suits <- rep(c('h','d','s','c'), each =13)
-cards <- rep(c('A',2:9, 'T', 'J','Q','K'),4)
-deck <- data.frame(cards, suits)
-deck <- unite(deck, col = "card", c(cards,suits), sep = "")
-
-# 5- or 6-Card Hand Inputted by the Player
-
-full_hand <- c('Ah', 'Ac', '3d', '2d', '6h', '6s')
-
-turn_cards <- vector()
-for (i in 1:52){
-  if (!(deck[i,] %in% full_hand)){
-    turn_cards <- c(turn_cards, deck[i,])
-  }
-}
-
-# For 5-Card Hands
-
-hand_eval <- data.frame(matrix(ncol = 8, nrow = 0))
-colnames(hand_eval) <- c("Hand","Discard","Average","Minimum",
-                         "Q1","Median","Q3","Maximum")
-
-if(length(full_hand) == 5){
-  for(i in 1:5){
-    hand <- full_hand[-i]
-    discard <- full_hand[i]
-    scores <- vector()
-    for(j in 1:47){
-      flipped <- turn_cards[j]
-      count_hand <- c(hand, flipped)
-      hand_score <- scored(count_hand)
-      scores[j] <- hand_score
+algorithm <- function(full_hand){
+  library(tidyr)
+  library(utils)
+  # Creating the 52-Card Deck (should already be loaded in the application)
+  suits <- rep(c('h','d','s','c'), each =13)
+  cards <- rep(c('A',2:9, 'T', 'J','Q','K'),4)
+  deck <- data.frame(cards, suits)
+  deck <- unite(deck, col = "card", c(cards,suits), sep = "")
+  
+  turn_cards <- vector()
+  for (i in 1:52){
+    if (!(deck[i,] %in% full_hand)){
+      turn_cards <- c(turn_cards, deck[i,])
     }
-    hand_eval[i,1] <- paste(hand, collapse = ", ")
-    hand_eval[i,2] <- discard
-    hand_eval[i,3] <- round(mean(scores),2)
-    hand_eval[i,4] <- fivenum(scores)[[1]]
-    hand_eval[i,5] <- fivenum(scores)[[2]]
-    hand_eval[i,6] <- fivenum(scores)[[3]]
-    hand_eval[i,7] <- fivenum(scores)[[4]]
-    hand_eval[i,8] <- fivenum(scores)[[5]]
-    hand_out <- arrange(hand_eval,desc(Average))
   }
-}
-
-# For 6-Card Hands
-
-if(length(full_hand == 6)){
-  for (i in 1:15){
-    hand <- combn(full_hand,4)[,i]
-    discard <- vector()
-    for (k in 1:6){
-      if (!(full_hand[k] %in% hand)){
-        discard <- c(discard, full_hand[k])
+  
+  if(full_hand[6] == ""){
+    full_hand <- full_hand[1:5]
+  }
+  
+  # For 5-Card Hands
+  
+  hand_eval <- data.frame(matrix(ncol = 8, nrow = 0))
+  colnames(hand_eval) <- c("Hand","Discard","Average","Minimum",
+                           "Q1","Median","Q3","Maximum")
+  
+  if(length(full_hand) == 5){
+    for(i in 1:5){
+      hand <- full_hand[-i]
+      discard <- full_hand[i]
+      scores <- vector()
+      for(j in 1:47){
+        flipped <- turn_cards[j]
+        count_hand <- c(hand, flipped)
+        hand_score <- scored(count_hand)
+        scores[j] <- hand_score
       }
+      hand_eval[i,1] <- paste(hand, collapse = ", ")
+      hand_eval[i,2] <- discard
+      hand_eval[i,3] <- round(mean(scores),2)
+      hand_eval[i,4] <- fivenum(scores)[[1]]
+      hand_eval[i,5] <- fivenum(scores)[[2]]
+      hand_eval[i,6] <- fivenum(scores)[[3]]
+      hand_eval[i,7] <- fivenum(scores)[[4]]
+      hand_eval[i,8] <- fivenum(scores)[[5]]
+      hand_out <- arrange(hand_eval,desc(Average))
     }
-    scores <- vector()
-    for(j in 1:46){
-      flipped <- turn_cards[j]
-      count_hand <- c(hand, flipped)
-      hand_score <- scored(count_hand)
-      scores[j] <- hand_score
-    }
-    hand_eval[i,1] <- paste(hand, collapse = ", ")
-    hand_eval[i,2] <- paste(discard, collapse = ", ")
-    hand_eval[i,3] <- round(mean(scores),2)
-    hand_eval[i,4] <- fivenum(scores)[[1]]
-    hand_eval[i,5] <- fivenum(scores)[[2]]
-    hand_eval[i,6] <- fivenum(scores)[[3]]
-    hand_eval[i,7] <- fivenum(scores)[[4]]
-    hand_eval[i,8] <- fivenum(scores)[[5]]
-    hand_out <- arrange(hand_eval,desc(Average))
   }
+  
+  # For 6-Card Hands
+  
+  else if(length(full_hand == 6)){
+    for (i in 1:15){
+      hand <- combn(full_hand,4)[,i]
+      discard <- vector()
+      for (k in 1:6){
+        if (!(full_hand[k] %in% hand)){
+          discard <- c(discard, full_hand[k])
+        }
+      }
+      scores <- vector()
+      for(j in 1:46){
+        flipped <- turn_cards[j]
+        count_hand <- c(hand, flipped)
+        hand_score <- scored(count_hand)
+        scores[j] <- hand_score
+      }
+      hand_eval[i,1] <- paste(hand, collapse = ", ")
+      hand_eval[i,2] <- paste(discard, collapse = ", ")
+      hand_eval[i,3] <- round(mean(scores),2)
+      hand_eval[i,4] <- fivenum(scores)[[1]]
+      hand_eval[i,5] <- fivenum(scores)[[2]]
+      hand_eval[i,6] <- fivenum(scores)[[3]]
+      hand_eval[i,7] <- fivenum(scores)[[4]]
+      hand_eval[i,8] <- fivenum(scores)[[5]]
+      hand_out <- arrange(hand_eval,desc(Average))
+    }
+  }
+  return(hand_out)
 }
--- shiny.crib.R
+++ shiny.crib.R
@@ -1,11 +1,94 @@
 library(shiny)
+library(tidyr)
+library(utils)
+library(dplyr)
+source('~/WORKING_DIRECTORIES/crib/scored.R')
+source('~/WORKING_DIRECTORIES/crib/algorithm.R')
+
+# Creating the 52-Card Deck
+suits <- rep(c('h','d','s','c'), each =13)
+cards <- rep(c('A',2:9, 'T', 'J','Q','K'),4)
+deck <- data.frame(cards, suits)
+deck <- unite(deck, col = "card", c(cards,suits), sep = "")
 
 ui <- fluidPage(
   
-  titlePanel("Cribbage Hand Evaluator")
-  
+  titlePanel("Cribbage Hand Evaluator"),
+  sidebarLayout(
+    sidebarPanel(
+      selectInput(inputId = "r1",
+                  label = "Card 1 Rank",
+                  choices = c("","Ace"="A","2","3","4","5","6","7","8","9",
+                              "10" = "T","Jack" = "J","Queen" = "Q","King" = "K")
+                  ),
+      selectInput(inputId = "s1",
+                  label = "Card 1 Suit",
+                  choices = c("","Club"="c","Spade" = "s","Heart" = "h","Diamond" = "d")
+                  ),
+      selectInput(inputId = "r2",
+                  label = "Card 2 Rank",
+                  choices = c("","Ace"="A","2","3","4","5","6","7","8","9",
+                              "10" = "T","Jack" = "J","Queen" = "Q","King" = "K")
+                  ),
+      selectInput(inputId = "s2",
+                  label = "Card 2 Suit",
+                  choices = c("","Club"="c","Spade" = "s","Heart" = "h","Diamond" = "d")
+                  ),
+      selectInput(inputId = "r3",
+                  label = "Card 3 Rank",
+                  choices = c("","Ace"="A","2","3","4","5","6","7","8","9",
+                              "10" = "T","Jack" = "J","Queen" = "Q","King" = "K")
+                  ),
+      selectInput(inputId = "s3",
+                  label = "Card 3 Suit",
+                  choices = c("","Club"="c","Spade" = "s","Heart" = "h","Diamond" = "d")
+                  ),
+      selectInput(inputId = "r4",
+                  label = "Card 4 Rank",
+                  choices = c("","Ace"="A","2","3","4","5","6","7","8","9",
+                              "10" = "T","Jack" = "J","Queen" = "Q","King" = "K")
+                  ),
+      selectInput(inputId = "s4",
+                  label = "Card 4 Suit",
+                  choices = c("","Club"="c","Spade" = "s","Heart" = "h","Diamond" = "d")
+                  ),
+      selectInput(inputId = "r5",
+                  label = "Card 5 Rank",
+                  choices = c("","Ace"="A","2","3","4","5","6","7","8","9",
+                              "10" = "T","Jack" = "J","Queen" = "Q","King" = "K")
+                  ),
+      selectInput(inputId = "s5",
+                  label = "Card 5 Suit",
+                  choices = c("","Club"="c","Spade" = "s","Heart" = "h","Diamond" = "d")
+                  ),
+      selectInput(inputId = "r6",
+                  label = "Card 6 Rank (if applicable)",
+                  choices = c("","Ace"="A","2","3","4","5","6","7","8","9",
+                              "10" = "T","Jack" = "J","Queen" = "Q","King" = "K")
+                  ),
+      selectInput(inputId = "s6",
+                  label = "Card 6 Suit (if applicable)",
+                  choices = c("","Club"="c","Spade" = "s","Heart" = "h","Diamond" = "d")
+                  )
+    ),
+    mainPanel(
+      tableOutput("eval")
+    )
+  )
 )
 
-server <- function(input, output){}
+server <- function(input, output){
+  
+    card1 <- reactive({paste(input$r1,input$s1,sep = "")})
+    card2 <- reactive({paste(input$r2,input$s2,sep = "")})
+    card3 <- reactive({paste(input$r3,input$s3,sep = "")})
+    card4 <- reactive({paste(input$r4,input$s4,sep = "")})
+    card5 <- reactive({paste(input$r5,input$s5,sep = "")})
+    card6 <- reactive({paste(input$r6,input$s6,sep = "")})
+    full_hand <- reactive({c(card1(),card2(),card3(),card4(),card5(),card6())})
+    hand_out <- reactive({algorithm(full_hand())})
+    output$eval <- renderTable(hand_out())
+    
+}
 
 shinyApp(ui = ui, server = server)
